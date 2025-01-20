98f58fbcf115c2d36cfe21a020b6b85f5c59742f
Ericka Ponce <erickaponce92@gmail.com>
2025-01-20 12:56:01 -0500

    add styles

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,12 +1,17 @@
 import type { MenuItem } from "../types";
 
 type MenuItemProps = {
-    item: MenuItem
+    item: MenuItem,
+    addItem: (item: MenuItem) => void
 }
 
-const MenuItem = ( {item} : MenuItemProps ) => {
+const MenuItem = ( {item, addItem} : MenuItemProps ) => {
     return (
-        <button className="border-2 border-cyan-400 hover:bg-cyan-200 w-full p-3 flex justify-between">
+        <button 
+            className="border-2 border-cyan-400 hover:bg-cyan-200 w-full p-3 
+            flex justify-between rounded-lg"
+            onClick={() => addItem(item)}
+        >
             <p>{item.name}</p>
             <p className="font-black">${item.price}</p>
         </button>
