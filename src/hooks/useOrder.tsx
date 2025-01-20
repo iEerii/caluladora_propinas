fabf4fc0df8394fa3efdea086c8be3f6f8dd17ea
Ericka Ponce <erickaponce92@gmail.com>
2025-01-20 12:36:24 -0500

    add elements to state useOrder

---
+++ src/hooks/useOrder.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { MenuItem, OrderItem } from "../types";
+
+const useOrder = () => {
+    const [order, setOrder] = useState<OrderItem[]>([]);
+    
+    const addItem = ( item : MenuItem) => {
+        
+        const newItem = {...item, quantity: 1}
+        setOrder([...order, newItem]);
+    }
+
+    return {
+        addItem
+    };
+}
+ 
+export default useOrder;
