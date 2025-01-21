dd2dafe875070e6b8a73a905391403e5ec642ed5
Ericka Ponce <erickaponce92@gmail.com>
2025-01-21 13:18:26 -0500

    add code to remove items from order

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,11 +1,12 @@
 import { formatCurrency } from "../helpers";
-import { OrderItem } from "../types";
+import { MenuItem, OrderItem } from "../types";
 
 type OrderContentsProps = {
-    order: OrderItem[]
+    order: OrderItem[],
+    removeItem: (id: MenuItem['id']) => void
 }
 
-const OrderContents = ({order} : OrderContentsProps) => {
+const OrderContents = ({order, removeItem} : OrderContentsProps) => {
     return (
         <div>
             <h2 className="font-black text-4xl">Consumo</h2>
@@ -29,6 +30,7 @@ const OrderContents = ({order} : OrderContentsProps) => {
                             </div>
                             <button
                                 className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
+                                onClick={() => removeItem(item.id)}
                             >
                                 X
                             </button>
--- src/hooks/useOrder.tsx
+++ src/hooks/useOrder.tsx
@@ -21,9 +21,14 @@ const useOrder = () => {
         }
     }
 
+    const removeItem = (id: MenuItem['id']) => {
+        setOrder(order.filter(item => item.id !== id))
+    }
+
     return {
         order,
-        addItem
+        addItem,
+        removeItem
     };
 }
  
